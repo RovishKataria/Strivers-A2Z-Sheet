af45549ca63da2459ff53d270d40f3bfe2188ad7
RovishKataria <rohankataria2000@gmail.com>
2025-08-04 02:58:31 +0530

    Step 14 - Lec 1

--- Step 14 - Binary Search Trees/Lec 1 Concepts/min_max_in_BST.cpp
+++ Step 14 - Binary Search Trees/Lec 1 Concepts/min_max_in_BST.cpp
@@ -1,3 +1,21 @@
 // https://www.naukri.com/code360/problems/minimum-element-in-bst_8160462
 // https://www.geeksforgeeks.org/problems/minimum-element-in-bst/1
 
+/*
+ * class Node
+ * {
+ * public:
+ *     int data;
+ *     Node *left;
+ *     Node *right;
+ *     Node() : data(0), left(nullptr), right(nullptr){};
+ *     Node(int x) : data(x), left(nullptr), right(nullptr) {}
+ *     Node(int x, Node *left, Node *right) : data(x), left(left), right(right) {}
+ * };
+ */
+int minVal(Node* root){
+	if (root == NULL) return -1;
+	Node* node = root;
+	while (node->left != NULL) node = node->left;
+	return node->data;	
+}
